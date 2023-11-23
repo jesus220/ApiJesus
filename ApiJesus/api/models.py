2f3f7125db7e84624418f246ad7b7e9da78cc6d0
Alfonso <Jesus460c@gmail.com>
2023-11-23 14:56:31 -0600

    add commit

--- ApiJesus/api/models.py
+++ ApiJesus/api/models.py
@@ -30,13 +30,13 @@ class General(models.Model):
         db_table="General"
         
         
-# class Product(models.Model):
+class Product(models.Model):
     
-#     name=models.CharField(max_length=225)
-#     description= models.TextField()
-#     price= models.IntegerField()
-#     image=models.URLField(blank=True, null=True)
+    name=models.CharField(max_length=225)
+    description= models.TextField()
+    price= models.IntegerField()
+    image=models.URLField(blank=True, null=True)
     
-#     def __str__(self) :
-#         return self.name
+    def __str__(self) :
+        return self.name
     
